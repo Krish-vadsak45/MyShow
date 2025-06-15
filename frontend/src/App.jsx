7d25e6ebc78b5fce8dda9a261d88715587b97173
krish-vadsak45 <krishvadsak234@gmail.com>
2025-06-15 23:26:49 +0530

    Add initial frontend structure with components, pages, and assets
    - Created Footer and Navbar components
    - Added pages: Home, Movies, MovieDetail, Favourite, MyBookings, and SeatLayout
    - Included SVG and PNG assets for profile and screen images
    - Set up Tailwind CSS and Vite configuration
    - Established main entry point with React Router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favourite from "./pages/Favourite";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+
+function App() {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
+  return (
+    <>
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/mybookings" element={<MyBookings />} />
+        <Route path="/favourite" element={<Favourite />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+}
+
+export default App;
