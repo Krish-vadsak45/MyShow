8d97d8a87dbaddb7ec4c569a74ca11a42e8125b5
krish-vadsak45 <krishvadsak234@gmail.com>
2025-06-23 17:54:36 +0530

    feat: implement email confirmation for bookings and add nodemailer dependency

--- backend/controllers/stripeWebhooks.js
+++ backend/controllers/stripeWebhooks.js
@@ -1,14 +1,12 @@
 import stripe from "stripe";
 import Booking from "../models/booking.model.js";
+import { inngest } from "../inngest/index.js";
 
 export const stripeWebhooks = async (req, res) => {
   const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
   const sig = req.headers["stripe-signature"];
-  console.log("1");
   let event;
   try {
-    console.log("2");
-
     event = stripeInstance.webhooks.constructEvent(
       req.body,
       sig,
@@ -17,33 +15,28 @@ export const stripeWebhooks = async (req, res) => {
   } catch (error) {
     return res.status(400).send("webhook error :- ", error);
   }
-  console.log("3");
 
   try {
-    console.log("4");
-
     switch (event.type) {
       case "payment_intent.succeeded": {
-        console.log("5");
-
         const paymentIntent = event.data.object;
         const sessionList = await stripeInstance.checkout.sessions.list({
           payment_intent: paymentIntent.id,
         });
-        console.log("6");
 
         const session = sessionList.data[0];
-        console.log(session);
-        console.log(session.metadata);
         const { bookingId } = session.metadata;
-        console.log(bookingId);
-        console.log("7");
 
         await Booking.findByIdAndUpdate(bookingId, {
           isPaid: true,
           paymentLink: "",
         });
-        console.log("8");
+
+        // Send Comformation email
+        await inngest.send({
+          name: "app/show.booked",
+          data: { bookingId },
+        });
 
         break;
       }
